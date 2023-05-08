3cd34693bd5a8526cfa58fef63596d884513dd47
Bruno Jacob <bruno.jacob.ext@ausy-technologies.de>
2023-05-08 08:35:52 +0100

    [SECURITY_001] - Adding a basic In Memory authentication

---
+++ src/main/java/org/exercise/travellers/configuration/PasswordEncoder.java
@@ -0,0 +1,14 @@
+package org.exercise.travellers.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordEncoder {
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/org/exercise/travellers/configuration/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package org.exercise.travellers.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${spring.security.user.name}")
+    private String username;
+
+    @Value("${spring.security.user.password}")
+    private String password;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails service = User.withUsername(username)
+                .password(passwordEncoder.encode(password))
+                .roles("SERVICE")
+                .build();
+        return new InMemoryUserDetailsManager(service);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable().cors().and()
+                .sessionManagement().sessionCreationPolicy(STATELESS)
+                .and().authorizeHttpRequests().anyRequest().authenticated()
+                .and().httpBasic()
+                .and().build();
+
+    }
+}
